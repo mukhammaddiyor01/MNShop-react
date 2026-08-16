f521e37e00cf6288a866b0476ba3dfb2f19fde87
MUKHAMMADDIYOR IBROKHIMOV <imrbek0208@gmail.com>
2026-08-16 18:26:01 +0900

    feat: configurations are made for two dashboards

--- src-seller/src/app/App.tsx
+++ src-seller/src/app/App.tsx
@@ -1,48 +1,40 @@
 import React from "react";
 import "../css/app.css";
-import { Box, Container, Stack, Typography } from "@mui/material";
-import { Link, Route, Switch, useLocation } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import { OverviewPage } from "./screens/overviewPage/index";
 import { MessagesPage } from "./screens/messagesPage/index";
 import { OrdersPage } from "./screens/ordersPage/index";
 import { AnalyticsPage } from "./screens/analyticsPage/index";
 import { ProductsPage } from "./screens/productsPage/index";
 import { SettingsPage } from "./screens/settingsPage/index";
-import { OtherNavbar } from "../../../src/app/components/headers/OtherNavbar";
 
 function App() {
-  const location = useLocation();
-  console.log(location);
   return (
     <>
       {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
       <Switch>
-        <Route path="seller/settings">
+        <Route path="/seller/settings">
           <SettingsPage />
         </Route>
-        <Route path="seller/analytics">
+        <Route path="/seller/analytics">
           <AnalyticsPage />
         </Route>
-        <Route path="seller/orders">
+        <Route path="/seller/orders">
           <OrdersPage />
         </Route>
-        <Route path="seller/messages">
+        <Route path="/seller/messages">
           <MessagesPage />
         </Route>
-        <Route path="seller/products">
+        <Route path="/seller/products">
           <ProductsPage />
         </Route>
-        <Route path="seller/">
+        <Route path="/seller/">
           <OverviewPage />
         </Route>
       </Switch>
     </>
   );
-}
-
-function Home() {
-  return <Container>Home</Container>;
 }
 
 export default App;
--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import "../css/app.css";
 import { Box, Container, Stack, Typography } from "@mui/material";
 import { Link, Route, Switch, useLocation } from "react-router-dom";
 import { HomePage } from "./screens/homePage";
@@ -10,7 +9,8 @@ import { AboutPage } from "./screens/aboutPage/index";
 import { HomeNavbar } from "./components/headers/HomeNavbar";
 import { OtherNavbar } from "./components/headers/OtherNavbar";
 import { Footer } from "./components/footer";
-import { OverviewPage } from "../../src-seller/src/app/screens/overviewPage/index";
+import "../css/navbar.css";
+import "../css/app.css";
 
 function App() {
   const location = useLocation();
@@ -22,6 +22,9 @@ function App() {
       {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
       <Switch>
+        <Route path="/orders">
+          <OrdersPage />
+        </Route>
         <Route path="/about">
           <AboutPage />
         </Route>
@@ -32,7 +35,7 @@ function App() {
           <ProductsPage />
         </Route>
         <Route path="/">
-          <OverviewPage />
+          <HomePage />
         </Route>
       </Switch>
       <Footer />
