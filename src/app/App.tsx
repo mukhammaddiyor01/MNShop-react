a17e4613cbb0ad4b4dc9993578325541fa1e93ce
MUKHAMMADDIYOR IBROKHIMOV <imrbek0208@gmail.com>
2026-08-16 16:17:43 +0900

    feat: create routing system

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,45 @@
 import React from "react";
 import "../css/app.css";
 import { Box, Container, Stack, Typography } from "@mui/material";
+import { Link, Route, Switch } from "react-router-dom";
+import { About } from "./screens/About";
+import { Users } from "./screens/Users";
 
 function App() {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/users">Users</Link>
+          </li>
+        </ul>
+      </nav>
+
+      {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+      <Switch>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/users">
+          <Users />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </div>
+  );
+}
+
+function Home() {
   return <Container>Home</Container>;
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,7 @@ import reportWebVitals from "./reportWebVitals";
 import CssBaseline from "@mui/material/CssBaseline";
 import { ThemeProvider } from "@mui/material/styles";
 import "./css/index.css";
+import { BrowserRouter as Router } from "react-router-dom";
 import theme from "./app/MaterialTheme copy";
 
 // Global integration => REDUX
@@ -15,7 +16,9 @@ ReactDOM.render(
     <Provider store={store}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        <App />
+        <Router>
+          <App />
+        </Router>
       </ThemeProvider>
     </Provider>
   </React.StrictMode>,
